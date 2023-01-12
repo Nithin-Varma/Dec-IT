089c8c4cc04ba93bf6cd79db86a97d7dfbf34a15
Nithin-Varma <mnithin1422@gmail.com>
2023-01-12 21:48:03 +0530

    choose img, upload, uploading images

--- decit/src/components/file.js
+++ decit/src/components/file.js
@@ -1,18 +1,59 @@
 import {useState, useEffect} from "react"
 import axios from "axios";
+import "./file.css";
 const File = ({contract, account, provider}) => {
 
+    const [file, setfile] = useState(null);
+    const [filename, setfilename] = useState("No file Selected");
 
-    const handleSubmit = async() => {
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        if(file) {
+            try {
+                const formData = new FormData();
+                formData.append("file", file);
+
+                const resfile = await axios({
+                    method: "post",
+                    url:"https://api.pinata.cloud/pinning/pinFileToIPFS",
+                    data: formData,
+                    headers: {
+                        pinata_api_key: `fed8d2362a1d40026b12`,
+                        pinata_secret_api_key: `70fd5e9072f82ce9755d6bc111be0cb803afe39c7d08eee6351f57d6d9abe9fc`,
+                        "Content-Type": "multipart/form-data",
+                    },
+                });
+
+                const ImgHash = `ipfs://${resfile.data.IpfsHash}`;
+                await contract.add(account,ImgHash)
+                alert("Uploaded Successfully");
+                setfilename("No file Selected");
+                setfile(null)
+
+
+            }
+            catch(error) {
+                alert("error while uploading image.", error)
+            }
+        }
 
     }
 
-    const retrievefile = () => {
+    const retrievefile = (e) => {
+        const data = e.target.files[0];
+        const reader = new window.FileReader();
+        reader.readAsArrayBuffer(data);
+        reader.onloadend = () => {
+            setfile(e.target.files[0]);
+        }
 
+        setfilename(e.target.files[0].name);
+        e.preventDefault();
     }
 
     return (
-        <div>
+        <div className="top">
     <form className = "form" onSubmit = {handleSubmit}>
         <label htmlFor="file-upload" className="choose">
             Choose Image
@@ -20,8 +61,8 @@ const File = ({contract, account, provider}) => {
 
         <input disabled={!account} type="file" id="file-upload" name = "data" onChange={retrievefile}/>
 
-        <span>Image: xxxxxxxx</span>
-        <button type="submit">Upload</button>
+        <span className="textArea">" {filename} " </span><br/><br/><br/>
+        <button type="submit" className="upload">Upload</button>
     </form>
     </div>
     )
